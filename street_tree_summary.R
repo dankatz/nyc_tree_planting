ab4621ce5a639cc130c96aea4484fa3af1339262
dankatz <dankatz512@gmail.com>
2023-03-28 18:04:54 -0400

    adding in street tree data

--- street_tree_summary.R
+++ street_tree_summary.R
@@ -1,1 +1,31 @@
-##
+#set up work environment
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(here)
+library(readr)
+library("stringr")
+
+
+
+#rm(list=ls())
+
+here::i_am("street_tree_summary.R") #using the 'here' package for consistent file structure system
+
+### load in street tree data and create derived vars ####################################
+st_trees <- read_csv( "C:/Users/dsk273/Box/NYC projects/tree data/2015 Street Tree Census - Tree Data/2015StreetTreesCensus_TREES.csv")
+st_trees <- st_trees %>% 
+  mutate(genus = sub(" .*", "", spc_latin),
+         dbh_cm = 2.54 * tree_dbh,
+         BA_m2 = 0.00007854 *  dbh_cm^2 ) %>% 
+  filter(!is.na(genus) &
+           status == "Alive")
+
+test$genus
+hist(st_trees$BA_m2)
+
+#export table with basal area 
+genus_summary <- st_trees %>%  group_by(genus) %>% 
+  summarize(total_BA = sum(BA_m2, na.rm = TRUE),
+            n_trees = n()) %>% 
+  arrange(total_BA)
